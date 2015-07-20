97ab7efefae2a7fe0490e574b772aeac289e0d17
Rafael César Neves <rafael.ceesar@gmail.com>
2015-07-20 01:43:13 -0300

    testes

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -22,12 +22,10 @@ class CandidatesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_candidate
       @candidate = Candidate.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def candidate_params
       params.require(:candidate).permit(:name, :email, :html_score, :css_score, :javascript_score, :python_score, :django_score, :ios_score, :android_score)
     end
