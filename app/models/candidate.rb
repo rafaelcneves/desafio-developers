3f3fc9b3e5b6eeba6c458d8340042e59cc8512f3
Rafael César Neves <rafael.ceesar@gmail.com>
2018-01-29 23:16:42 -0200

    Updating packages

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,4 @@
-class Candidate < ActiveRecord::Base
+class Candidate < ApplicationRecord
   validates :name, :email, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :html_score, :css_score, :javascript_score, :python_score, :django_score, :ios_score, :android_score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }, allow_nil: true
